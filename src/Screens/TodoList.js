d1906e3bb55c33a916e4c20f3f5dcc717c1c167c
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2022-01-16 20:53:28 +0500

    Added Todo Create Functionality + UI changes

--- src/Screens/TodoList.js
+++ src/Screens/TodoList.js
@@ -1,7 +1,20 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+  Dimensions,
+  StatusBar,
+} from 'react-native';
 import axios from 'axios';
 import {COLORS} from '../utils/colors';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import Modal from 'react-native-modal';
+
+const width = Dimensions.get('window').width;
 
 const API_BASE = 'http://192.168.0.111:3001';
 
@@ -18,7 +31,6 @@ const TodoList = () => {
     axios
       .get(`${API_BASE}/todos`)
       .then(response => {
-        // console.log('response ==> ', response.data);
         setTodos(response.data);
       })
       .catch(err => console.error('Error: ', err));
@@ -44,8 +56,25 @@ const TodoList = () => {
     setTodos(todos => todos.filter(todo => todo._id !== data._id));
   };
 
+  const addTodo = async () => {
+    await axios
+      .post(`${API_BASE}/todo/new`, {
+        text: newTodo,
+      })
+      .then(function (response) {
+        const data = response.data;
+        setTodos([...todos, data]);
+        setModalActive(false);
+        setNewTodo('');
+      })
+      .catch(function (error) {
+        console.log('Error: ', error);
+      });
+  };
+
   return (
     <View style={styles.container}>
+      <StatusBar backgroundColor={COLORS.DARKALT} />
       <Text style={styles.heading}>Welcome to Todo List App!</Text>
       <Text style={styles.heading}>Your Tasks</Text>
 
@@ -69,6 +98,37 @@ const TodoList = () => {
           )}
         />
       </View>
+
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => {
+          setModalActive(true);
+        }}>
+        <MaterialIcon name="add" size={32} color={COLORS.LIGHT} />
+      </TouchableOpacity>
+
+      <Modal
+        isVisible={modalActive}
+        animationIn={'slideInUp'}
+        animationOut={'slideInDown'}>
+        <View style={styles.modalView}>
+          <TouchableOpacity
+            style={styles.modalCloseBtn}
+            onPress={() => setModalActive(false)}>
+            <Text style={styles.modalCloseBtnText}>X</Text>
+          </TouchableOpacity>
+          <Text style={styles.addTaskHeading}>Add Task</Text>
+          <TextInput
+            style={styles.taskInput}
+            placeholder="Enter task here.."
+            onChangeText={text => setNewTodo(text)}
+            value={newTodo}
+          />
+          <TouchableOpacity style={styles.createBtn} onPress={() => addTodo()}>
+            <Text style={styles.createBtnText}>Create Task</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -121,5 +181,70 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
     backgroundColor: COLORS.DARK,
+  },
+  addButton: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 64,
+    height: 64,
+    borderRadius: 90,
+    backgroundColor: COLORS.PRIMARY,
+    elevation: 1,
+  },
+  addButtonText: {
+    fontSize: 28,
+    fontWeight: '900',
+    color: COLORS.LIGHT,
+  },
+  modalView: {
+    backgroundColor: COLORS.LIGHT,
+    borderRadius: 40,
+    width: width * 0.9,
+    height: 400,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalCloseBtn: {
+    backgroundColor: 'red',
+    padding: 10,
+    margin: 10,
+    width: 40,
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    borderRadius: 20,
+  },
+  modalCloseBtnText: {color: COLORS.WHITE, fontSize: 18, textAlign: 'center'},
+  addTaskHeading: {
+    color: COLORS.DARK,
+    marginBottom: 16,
+    fontWeight: '400',
+    textTransform: 'uppercase',
+    fontSize: 25,
+  },
+  taskInput: {
+    backgroundColor: COLORS.WHITE,
+    width: width * 0.7,
+    padding: 16,
+    borderRadius: 16,
+    elevation: 2,
+  },
+  createBtn: {
+    backgroundColor: COLORS.PRIMARY,
+    paddingTop: 16,
+    paddingBottom: 16,
+    paddingLeft: 32,
+    paddingRight: 32,
+    borderRadius: 90,
+    marginTop: 16,
+  },
+  createBtnText: {
+    fontSize: 18,
+    fontWeight: '700',
+    textAlign: 'center',
+    color: COLORS.WHITE,
   },
 });
